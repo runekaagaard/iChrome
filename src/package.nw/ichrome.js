236ff2015ba52c179d91368ae5f502c830c63829
Rune Kaagaard <rumi.kg@gmail.com>
2023-02-23 14:42:35 +0100

    Most functionality is in place.
    - Can both zoom on nw.js AND webview level.
    - Device picker
    - Url chooser.
    
    Going forward we need:
    
    - Sensible zoom defaults for all devices.
    - Persistent state that remember last url globally and zoom levels per device.
    - Better looking UI.
    - Button that shows/hides UI.
    - Debug why some devices can't scroll.
    - Test all devices.
    - Release builds for linux, windows and osx.

--- src/package.nw/ichrome.js
+++ src/package.nw/ichrome.js
@@ -1,14 +1,31 @@
+var zoom = 1.0
+
 function icons() {
   var win = nw.Window.get()
   
-  document.getElementById("plus").addEventListener("click", function() {
+  document.getElementById("nw-zoom-up").addEventListener("click", function() {
     win.zoomLevel += 0.1
   })
-  document.getElementById("minus").addEventListener("click", function() {
+  document.getElementById("nw-zoom-down").addEventListener("click", function() {
     win.zoomLevel -= 0.1
+  })
+  document.getElementById("webview-zoom-up").addEventListener("click", function() {
+    zoom += 0.02
+    document.getElementById("webview").setZoom(window.zoom + 0.1)
+  })
+  document.getElementById("webview-zoom-down").addEventListener("click", function() {
+    zoom -= 0.02
+    document.getElementById("webview").setZoom(window.zoom + 0.1)
   })
   document.getElementById("fullscreen").addEventListener("click", function() {
     win.toggleFullscreen()
+  })
+  document.getElementById("choose-device").addEventListener("change", function(e) {
+    console.log(e, e.target.value)
+    document.getElementById("device").className = "device "+ e.target.value
+  })
+  document.getElementById("go").addEventListener("click", function(e) {
+    document.getElementById("webview").setAttribute("src", document.getElementById("url").value)
   })
 }
 
@@ -26,7 +43,7 @@ function main() {
     var win = nw.Window.get()
     win.zoomLevel += 0.1
     win.zoomLevel -= 0.1
-  }, 0)
+  }, 100)
 }
 
 main()
