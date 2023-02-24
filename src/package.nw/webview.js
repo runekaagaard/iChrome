0c0c7f6e58216dcc908de1a7a42711722ff1fde0
Rune Kaagaard <rumi.kg@gmail.com>
2023-02-24 12:50:12 +0100

    Remove unused file.

--- src/package.nw/webview.js
+++
@@ -1,7 +0,0 @@
-
-// Continously hides the scrollbar inside the webview tag.
-function allYourBarAreBelongToUs() {
-  document.head.insertAdjacentHTML(
-    "beforeend",
-    `<style id="hide-scrollbar">*::-webkit-scrollbar { display: none !important; }</style>`)
-}
