e5f49722187f5de4cce753f3e29462e59edfe598
Rune Kaagaard <rumi.kg@gmail.com>
2023-02-24 09:48:47 +0100

    Fixing minor css problems. Adding back, forward and colorpicker commands.

--- src/package.nw/ichrome.js
+++ src/package.nw/ichrome.js
@@ -1,9 +1,21 @@
+// state //
+
+// https://nwjs.readthedocs.io/en/latest/References/Window/
 const win = nw.Window.get()
 const zooms = {
   nw: 0,
   wv: 1,
 }
+// https://developer.chrome.com/docs/extensions/reference/webviewTag/
 const wv = document.getElementById("webview")
+
+// utils //
+
+function event(id, event_type, func) {
+  document.getElementById(id).addEventListener(event_type, func)
+}
+
+// commands //
 
 function nwZoom() { win.zoomLevel = zooms.nw }
 function nwZoomOut() { zooms.nw -= 0.05; nwZoom() }
@@ -12,44 +24,48 @@ function wvZoom() { wv.setZoom(zooms.wv) }
 function wvZoomOut() { zooms.wv -= 0.02; wvZoom() }
 function wvZoomIn() { zooms.wv += 0.02; wvZoom() }
 function wvReload() { wv.setAttribute("src", wv.getAttribute("src")) }
+function wvBack() { wv.back() }
+function wvForward() { wv.forward() }
+function toggleFullscreen() { win.toggleFullscreen() }
+function destroyScrollbar() { wv.insertCSS({code: "body::-webkit-scrollbar { display: none; }"}) }
 
-function icons() {
-  document.getElementById("nw-zoom-out").addEventListener("click", nwZoomOut)
-  document.getElementById("nw-zoom-in").addEventListener("click", nwZoomIn)
-  document.getElementById("webview-zoom-out").addEventListener("click", wvZoomOut)
-  document.getElementById("webview-zoom-in").addEventListener("click", wvZoomIn)
-  document.getElementById("fullscreen").addEventListener("click", () => win.toggleFullscreen())
-  document.getElementById("reload").addEventListener("click", wvReload)
-  document.getElementById("choose-device").addEventListener("change", function(e) {
-    document.getElementById("device").className = "device "+ e.target.value
-  })
-  document.getElementById("go").addEventListener("click", function(e) {
-    wv.setAttribute("src", document.getElementById("url").value)
-  })
-}
+// app //
 
-function shortcuts() {
-  for (const [key, active] of [
-    ["Ctrl+Shift+P", nwZoomIn],
-    ["Ctrl+Shift+M", nwZoomOut],
-    ["Ctrl+Shift+I", wvZoomIn],
-    ["Ctrl+Shift+O", wvZoomOut],
-    ["Ctrl+Shift+S", () => { win.toggleFullscreen()}],
-    ["Ctrl+Shift+R", wvReload],
+function commands() {
+  // Register icon clicks and keyboard shortcuts.
+  for (const [key, id, func] of [
+    ["Ctrl+Shift+M", "nw-zoom-out", nwZoomOut],
+    ["Ctrl+Shift+P", "nw-zoom-in", nwZoomIn],
+    ["Ctrl+Shift+O", "webview-zoom-out", wvZoomOut],
+    ["Ctrl+Shift+I", "webview-zoom-in", wvZoomIn],
+    ["Ctrl+Shift+A", "back", wvBack],
+    ["Ctrl+Shift+B", "forward", wvForward],
+    ["Ctrl+Shift+S", "fullscreen", toggleFullscreen],
+    ["Ctrl+Shift+R", "reload", wvReload],
   ]) {
-    nw.App.registerGlobalHotKey(new nw.Shortcut({key, active}));
+    event(id, "click", func)
+    nw.App.registerGlobalHotKey(new nw.Shortcut({key, active: func}));
   }
+
+  // Other commands
+  event("choose-device", "change", (e) =>
+    document.getElementById("device").className = "device "+ e.target.value
+  )
+  event("go", "click", (e) => {
+    wv.setAttribute("src", document.getElementById("url").value)
+    destroyScrollbar()
+  })
+  event("color", "input", (e) =>
+    document.querySelector("body").style["background-color"] = e.target.value
+  )
 }
 
 function main() {
   // Allow video
-  document.getElementById("device").addEventListener('permissionrequest', function permissionRequest(e) {
-    e.request.allow();
-  })
+  event("device", "permissionrequest", (e) => e.request.allow())
 
-  // Register icon clicks and keyboard shortcuts()
-  icons()
-  shortcuts()
+  // Register icon clicks and keyboard commands()
+  commands()
 
   // Fixes that sometimes the zoomlevel inside the webview is wrong until the zoomlevel is changed.
   setTimeout(function() {
@@ -58,7 +74,7 @@ function main() {
   }, 100)
 
   // Remove scrollbar in webview context.
-  wv.insertCSS({code: "body::-webkit-scrollbar { display: none; }"})
+  destroyScrollbar()
 }
 
 main()
